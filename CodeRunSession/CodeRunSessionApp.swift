d69c7551008731b3188d8ae8a856116210b5cfb1
nectorprog <nector.dev@gmail.com>
2024-07-09 09:39:07 +0300

    Initial Commit

---
+++ CodeRunSession/CodeRunSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeRunSessionApp.swift
+//  CodeRunSession
+//
+//  Created by Владислав Пономаренко on 09.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CodeRunSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
