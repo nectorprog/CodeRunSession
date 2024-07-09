12be17868bf3da702928dda133cb1be3cfd53442
nectorprog <nector.dev@gmail.com>
2024-07-09 11:22:11 +0300

    build delete, 190 add

--- CodeRunSession/Mobile/180.swift
+++ CodeRunSession/Mobile/180.swift
@@ -1,46 +1,21 @@
 import Foundation
 
-class DSU {
-    private var parent: [Int: Int] = [:]
-
-    func find(_ x: Int) -> Int {
-        if parent[x] == nil {
-            parent[x] = x
-        }
-        if parent[x] != x {
-            parent[x] = find(parent[x]!)
-        }
-        return parent[x]!
-    }
-
-    func union(_ x: Int, _ y: Int) {
-        let rootx = find(x)
-        let rooty = find(y)
-        if rootx != rooty {
-            parent[rootx] = rooty
-        }
-    }
-}
-
 let n = Int(readLine()!)!
-let dsu = DSU()
+let str = readLine()!
+let lst = readLine()!.split(separator: " ").compactMap({Int($0)})
 
-for i in 0..<n {
-    let input = readLine()!.split(separator: " ").compactMap{Int($0)}
-    dsu.union(input[0], input[1])
+var maxRazn = lst[0]
+var maxRaznI = 0
+
+for i in 1..<n {
+    if lst[i] - lst[i-1] >= maxRazn {
+        maxRazn = lst[i] - lst[i-1]
+        maxRaznI = i
+    }
 }
 
-let t = Int(readLine()!)!
+var strIndex = str.index(str.startIndex, offsetBy: maxRaznI)
 
-for _ in 0..<t {
-    let query = readLine()!.split(separator: " ").compactMap{Int($0)}
-    let x = query[0]
-    let k = query[1]
+var strEl = str[strIndex]
 
-    let stores = readLine()!.split(separator: " ").compactMap{Int($0)}
-    let xRoot = dsu.find(x)
-
-    let filteredStore = stores.filter { dsu.find($0) == xRoot }
-
-    print(filteredStore.isEmpty ? 0 : "\(filteredStore.count) \(filteredStore.map{String($0)}.joined(separator: " "))")
-}
+print(strEl)
